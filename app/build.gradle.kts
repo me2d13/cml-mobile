7fb3591fdcc95d1d2d177fb0ab53534b2d88980e
Petr Medek <me2d@me2d.eu>
2025-09-01 20:49:10 +0200

    Version 2.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "eu.me2d.cmlmobile"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20000
-        versionName = "2.0.0"
+        versionCode = 20010
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
